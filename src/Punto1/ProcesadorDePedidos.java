52b8c94064f64b7dc7b3ba18397bc13f8f869635
marianachca <marianachca@unisabana.edu.co>
2024-02-12 21:07:12 -0500

    Se definen los pasos para hacer un pedido

--- src/Punto1/ProcesadorDePedidos.java
+++ src/Punto1/ProcesadorDePedidos.java
@@ -1,4 +1,21 @@
 package Punto1;
+abstract class ProcesadorDePedidos {
 
-public class ProcesadorDePedidos {
+    public final void procesarPedido(){
+
+        tomarPedido();
+        verificarDisponibilidadPedido();
+        calcularPrecioPedido();
+        prepararPedido();
+        enviarPedido();
+        verificarEntregaPedido();
+    }
+
+    abstract void tomarPedido();
+    abstract void verificarDisponibilidadPedido();
+    abstract void calcularPrecioPedido();
+    abstract void prepararPedido();
+    abstract void enviarPedido();
+    abstract void verificarEntregaPedido();
+
 }
