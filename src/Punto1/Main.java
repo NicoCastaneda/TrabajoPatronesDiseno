f8d0eec0b740ee703374f5dafbaacdfd0b8220b3
marianachca <marianachca@unisabana.edu.co>
2024-02-12 21:29:16 -0500

    Se completa clase cajita feliz

--- src/Punto1/Main.java
+++ src/Punto1/Main.java
@@ -1,4 +1,14 @@
 package Punto1;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        ProcesadorDePedidos pedirCajitaFeliz = new PedidoCajitaFeliz();
+        pedirCajitaFeliz.procesarPedido();
+
+        System.out.println("Pedido completado exitosamente");
+
+
+    }
 }
--- src/Punto1/PedidoCajitaFeliz.java
+++ src/Punto1/PedidoCajitaFeliz.java
@@ -3,31 +3,36 @@ package Punto1;
 public class PedidoCajitaFeliz extends ProcesadorDePedidos {
     @Override
     void tomarPedido() {
-
+        System.out.println("Tomando pedido... Cajita feliz");
     }
 
     @Override
     void verificarDisponibilidadPedido() {
+        System.out.println("Producto disponible");
 
     }
 
     @Override
     void calcularPrecioPedido() {
+        System.out.println("Precio: $22.500");
 
     }
 
     @Override
     void prepararPedido() {
+        System.out.println("Preparando cajita feliz");
 
     }
 
     @Override
     void enviarPedido() {
+        System.out.println("El pedido esta en camino");
 
     }
 
     @Override
     void verificarEntregaPedido() {
+        System.out.println("El pedido fue entregado");
 
     }
 }
